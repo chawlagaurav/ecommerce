eafcfaef44c713215b8f74da178a7403d342146e
Gaurav Chawla <84847577+chawlagaurav@users.noreply.github.com>
2022-05-07 18:32:50 +0530

    Update stripe.js

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
         shipping_options: [
-          { shipping_rate: 'shr_1Kwko3SDtYUtEZzVBobr8QaU' },
+          { shipping_rate: 'shr_1KwnHBSDtYUtEZzVSib4aX3v' },
         ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
